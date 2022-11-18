ed67957f5a6eade6a0bba83eefdb6e9cd267c43f
LilJay <j.davis@unadeca.net>
2022-11-17 20:46:20 -0600

    navbar simple changes

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#aac5bd]'>LOGO</h1>
-            <ul>
-                <li className=''>Inicio</li>
-                <li className=''>Conócenos</li>
-                <li className=''>Servicios</li>
-                <li className=''>Enlaces</li>
-                <li className=''>Contáctenos</li>
+        <div className='flex justify-between items-center h-24 w-full mx-auto text-white bg-slate-600 px-4'>
+            <h1 className='w-full text-3xl font-bold text-[#aac5bd]'>EL LOGO</h1>
+            <ul className='flex justify-between items-center'>
+                <li className='p-4'>Inicio</li>
+                <li className='p-4'>Conócenos</li>
+                <li className='p-4'>Servicios</li>
+                <li className='p-4'>Enlaces</li>
+                <li className='p-4'>Contáctenos</li>
             </ul>
         </div>
   )
